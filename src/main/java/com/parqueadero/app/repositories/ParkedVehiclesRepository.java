45a3a57bc51e92bf59635dbeebc1ef70c8a2a97b
holacomote2306 <holacomoteva2306@hotmail.com>
2024-08-13 08:19:35 -0500

    Commit casa

--- src/main/java/com/parqueadero/app/repositories/ParkedVehiclesRepository.java
+++ src/main/java/com/parqueadero/app/repositories/ParkedVehiclesRepository.java
@@ -13,6 +13,8 @@ public interface ParkedVehiclesRepository extends JpaRepository<ParkedVehiclesEn
 
     Optional<ParkedVehiclesEntity> findByCarPlateAndDepartureDateIsNull(String carPlate);
 
+    Optional<ParkedVehiclesEntity> findByCarPlate(String carPlate);
+
     @Query(value = "SELECT COUNT(pv) FROM ParkedVehiclesEntity pv " + 
         "WHERE pv.parkingLotEntity.id = :idParkingLot " +
         "AND pv.departureDate IS NULL ")
--- src/main/java/com/parqueadero/app/services/ParkedVehiclesServiceImpl.java
+++ src/main/java/com/parqueadero/app/services/ParkedVehiclesServiceImpl.java
@@ -1,10 +1,13 @@
 package com.parqueadero.app.services;
 
+import java.time.LocalDateTime;
+
 import org.springframework.stereotype.Service;
 
 import com.parqueadero.app.dtos.requests.ParkedVehicleRequest;
 import com.parqueadero.app.dtos.responses.ParkedVehicleResponse;
 import com.parqueadero.app.exceptions.BadRequestException;
+import com.parqueadero.app.exceptions.NotFoundException;
 import com.parqueadero.app.models.Audit;
 import com.parqueadero.app.models.ParkedVehiclesEntity;
 import com.parqueadero.app.models.ParkingLotEntity;
@@ -24,19 +27,20 @@ public class ParkedVehiclesServiceImpl implements IParkedVehiclesService {
 
     @Override
     public ParkedVehicleResponse registerVehicle(ParkedVehicleRequest parkedVehicleRequest) {
-        ParkingLotEntity parkingLotEntity = this.parkingLotService.findParkingLotById(parkedVehicleRequest.getIdParkingLot());
+        ParkingLotEntity parkingLotEntity = this.parkingLotService
+                .findParkingLotById(parkedVehicleRequest.getIdParkingLot());
 
         this.validIfExisteVehicleByCarPlate(parkedVehicleRequest.getCarPlate());
 
-        if(parkingLotEntity.getCapacity() <= this.countParkedVehiclesByParkingLotId(parkingLotEntity.getId())) {
+        if (parkingLotEntity.getCapacity() <= this.countParkedVehiclesByParkingLotId(parkingLotEntity.getId())) {
             throw new BadRequestException("capcity", "There are no spaces in the parking lot");
         }
 
         ParkedVehiclesEntity parkedVehiclesEntity = ParkedVehiclesEntity.builder()
-                                                        .parkingLotEntity(parkingLotEntity)
-                                                        .carPlate(parkedVehicleRequest.getCarPlate().toUpperCase())
-                                                        .audit(new Audit())
-                                                        .build();
+                .parkingLotEntity(parkingLotEntity)
+                .carPlate(parkedVehicleRequest.getCarPlate().toUpperCase())
+                .audit(new Audit())
+                .build();
 
         parkedVehiclesEntity = this.parkedVehiclesRepository.save(parkedVehiclesEntity);
         return new ParkedVehicleResponse(parkedVehiclesEntity);
@@ -44,19 +48,15 @@ public class ParkedVehiclesServiceImpl implements IParkedVehiclesService {
 
     @Override
     public ParkedVehicleResponse registerDepartureVehicle(ParkedVehicleRequest parkedVehicleRequest) {
-        ParkingLotEntity parkingLotEntity = this.parkingLotService.findParkingLotById(parkedVehicleRequest.getIdParkingLot());
+        ParkingLotEntity parkingLotEntity = this.parkingLotService
+                .findParkingLotById(parkedVehicleRequest.getIdParkingLot());
 
-        this.validIfExisteVehicleByCarPlate(parkedVehicleRequest.getCarPlate());
+        ParkedVehiclesEntity parkedVehiclesEntity = this.parkedVehiclesRepository
+                .findByCarPlateAndDepartureDateIsNull(parkedVehicleRequest.getCarPlate())
+                .orElseThrow(() -> new NotFoundException("carPlate", "A vehicle with that car plate was not found."));
 
-        if(parkingLotEntity.getCapacity() <= this.countParkedVehiclesByParkingLotId(parkingLotEntity.getId())) {
-            throw new BadRequestException("capcity", "There are no spaces in the parking lot");
-        }
-
-        ParkedVehiclesEntity parkedVehiclesEntity = ParkedVehiclesEntity.builder()
-                                                        .parkingLotEntity(parkingLotEntity)
-                                                        .carPlate(parkedVehicleRequest.getCarPlate().toUpperCase())
-                                                        .audit(new Audit())
-                                                        .build();
+        parkedVehiclesEntity.setDepartureDate(LocalDateTime.now().withNano(0).withSecond(0));
+        parkedVehiclesEntity.setTimeValue(this.);
 
         parkedVehiclesEntity = this.parkedVehiclesRepository.save(parkedVehiclesEntity);
         return new ParkedVehicleResponse(parkedVehiclesEntity);
@@ -65,14 +65,15 @@ public class ParkedVehiclesServiceImpl implements IParkedVehiclesService {
     @Override
     public void validIfExisteVehicleByCarPlate(String carPlate) {
         this.parkedVehiclesRepository.findByCarPlateAndDepartureDateIsNull(carPlate.toUpperCase()).ifPresent(
-            v -> {
-                throw new BadRequestException("carPlate", "There is a vehicle registered with that car plate.");
-            }
-        );
+                v -> {
+                    throw new BadRequestException("carPlate", "There is a vehicle registered with that car plate.");
+                });
     }
 
     @Override
     public Long countParkedVehiclesByParkingLotId(Long idParkingLot) {
         return this.parkedVehiclesRepository.countParkedVehiclesByParkingLotId(idParkingLot);
     }
+
+    private Long calculateTimeValue
 }
